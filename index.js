37763aa24158bf525ca80a524145efdb7f9abbf7
Mallikarjuna_Reddy_25 <137518088+Karna2506@users.noreply.github.com>
2024-09-29 20:04:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+        //Task 1
+
+
+for(var i=1; i <= 10; i++){
+    console.log(i);  
+}
+
+    // Task 2
+
+
+num =5;
+for(i = 1; i <= 10; i++){
+    console.log( num  + "X" + i + "=" +  (num*i) );
+}
+
+      //Task 3
+
+
+let sum=0;
+for(i=1; i<=10; i++){
+    sum+=i;
+}
+console.log( "The Sum of First 10 Natural Numbers = " + sum);
+
+
+
+//Task 4
+
+
+for(i=3;i<=30;i+=3){
+    console.log(i);   
+}
+
+      //Task 5
+
+      
+for(i=1;i<=10;i++){
+    console.log(i * i);
+    
+}
